9c5f50da47552113cd7df6ee808208b150c23b85
Quan Pham <quanicus@Quans-MacBook-Pro-2.local>
2024-06-11 02:14:49 -0500

    add debug console.log at server listening

--- wsServer.js
+++ wsServer.js
@@ -144,6 +144,7 @@ const wsServer = require("uWebSockets.js").App().ws("/*", {
 
 const PORT = process.env.PORT || 9001;
 wsServer.listen(PORT, (listenSocket) => {
+    console.log("are we listening?");
     if (listenSocket) {
         console.log(`WebSocket server listening on port ${PORT}`);
     }
